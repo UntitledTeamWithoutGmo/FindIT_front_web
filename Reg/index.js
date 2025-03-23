05339ec79343554626be2c2d6c52e17b50e78bb5
TeRrAiSt28 <28022012yura@gmail.com>
2025-03-23 12:37:30 +0200

    StartOfAll

---
+++ Reg/index.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('registrationForm');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const formData = {
+    name: document.getElementById('name').value,
+    username: document.getElementById('username').value,
+    surname: document.getElementById('surname').value,
+    patronymicName: document.getElementById('patronymicName').value,
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value
+  };
+
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if (res.ok) {
+      alert('Registration successful!');
+    } else {
+      alert('Registration failed.');
+    }
+  } catch (err) {
+    console.log(err.message);
+    alert('An error occurred.');
+  }
+});
